73cc403b71bbcafd5b34c8f18c3b1ea7921d73a5
William K <wkategianes@gmail.com>
2022-05-15 05:12:51 -0700

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // This activity allows the user to roll a dice and view the result on the screen /
+        val rollButton: Button = findViewById(R.id.rollButton)
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+            rollDice()
+        }
+    }
+
+    // Roll the dice and update the screen with the result. /
+    private fun rollDice() {
+
+        // Create new Dice object with 6 sides and roll it /
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll /
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+
+    }
+}
+
+class Dice(private val numSides: Int){
+
+    fun roll(): Int{
+        return(1..numSides).random()
+    }
+}
